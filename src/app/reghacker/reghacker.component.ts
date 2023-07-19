f31041ce5a3a8d29c9f4c101c6bca41258a2de24
Mpholess <gmpho0121@gmail.com>
2023-07-19 12:38:21 +0200

    New REcord

---
+++ src/app/reghacker/reghacker.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { SityService } from '../services/sity.service';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-reghacker',
+  templateUrl: './reghacker.component.html',
+  styleUrls: ['./reghacker.component.css']
+})
+export class ReghackerComponent {
+
+  hackerRegister= {
+    stu_no: "",
+    stu_name:"",
+    stu_surname: "",
+    email: "",
+    group_name : "",
+    password:"",
+    passwordConfirm:"",
+    group_id:"",
+
+  }
+
+  constructor(private _router:Router,private sityservice: SityService ){};
+
+  register(){
+    console.log(this.hackerRegister)
+    this.sityservice.hackerRegister(this.hackerRegister).subscribe((respond)=>{
+      console.log(respond)
+    },(error)=>{
+      console.log(error)
+    })
+  }
+}
---
+++ src/app/regjudge/regjudge.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { SityService } from '../services/sity.service';
+@Component({
+  selector: 'app-regjudge',
+  templateUrl: './regjudge.component.html',
+  styleUrls: ['./regjudge.component.css']
+})
+export class RegjudgeComponent {
+  judgeRegister= {
+    judge_name: "",
+    judge_surname:"",
+    email: "",
+    company_name: "",
+    password : "",
+    passwordConfirm:"",
+    Admin_id:"",
+    judge_id:"",
+    
+  }
+
+  constructor(private _router:Router,private sityservice: SityService ){};
+
+  register(){
+    console.log(this.judgeRegister)
+    this.sityservice.judgeRegister(this.judgeRegister).subscribe((respond)=>{
+      console.log(respond)
+    },(error)=>{
+      console.log(error)
+    })
+  }
+
+}
