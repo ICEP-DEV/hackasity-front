a5271f8683420b906ea49580dddae6181d812aeb
unknown <gmpho0121@gmail.com>
2023-07-20 14:06:13 +0200

    Updated

--- src/app/files/files.component.ts
+++ src/app/files/files.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import swal from "sweetalert2";
+import Swal from 'sweetalert2';
 import { SityService} from '../services/sity.service'
 
 @Component({
@@ -23,75 +23,72 @@ export class FilesComponent {
   onHacker(hacker : string){
     this._router.navigate(['/'+hacker])
   }
-   
-  // simpleAlert(){
-  //   swal.fire("hellow world")
-  // }
+
  
 
   register(){
     
-    var decimal = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
-    console.log(this.adminRegister.password)
-    if (!this.adminRegister.password.match(decimal)) {
-      swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'password is not strong!',
-        footer: '<label>To check a password between 8 to 15 characters which contain at least one lowercase letter, one uppercase letter, one numeric digit </br>and one special character</label>'
-      })
-      return
-    }
-
-    if (this.adminRegister.name == "") {
-      swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Name is required!',
-      })
-      return
-      
-    }
-    if (this.adminRegister.surname == "") {
-      swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Surname is required!',
-      })
-      return
-    }
-    if (this.adminRegister.email == "") {
-      swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Email is required!',
-      })
-      return
-    }
-    if (this.adminRegister.password == "") {
-      swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Password is required!',
-      })
-      return
-    }
-    if (this.adminRegister.passwordConfirm == "") {
-      swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Password confirmation is required!',
-      })
-      return
-    }
-  console.log(this.adminRegister)
-  this.sityservice.adminRegister(this.adminRegister).subscribe((respond)=>{
-    console.log(respond)
-    swal.fire("Thank You...", 'You registered sucessfully', 'success')
-  },(error)=>{
-    console.log(error)
-  })
-}
+      var decimal = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+      console.log(this.adminRegister.password)
+      if (!this.adminRegister.password.match(decimal)) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'password is not strong!',
+          footer: '<label>To check a password between 8 to 15 characters which contain at least one lowercase letter, one uppercase letter, one numeric digit </br>and one special character</label>'
+        })
+        return
+      }
+  
+      if (this.adminRegister.name == "") {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Name is required!',
+        })
+        return
+        
+      }
+      if (this.adminRegister.surname == "") {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Surname is required!',
+        })
+        return
+      }
+      if (this.adminRegister.email == "") {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Email is required!',
+        })
+        return
+      }
+      if (this.adminRegister.password == "") {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Password is required!',
+        })
+        return
+      }
+      if (this.adminRegister.passwordConfirm == "") {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Password confirmation is required!',
+        })
+        return
+      }
+    console.log(this.adminRegister)
+    this.sityservice.adminRegister(this.adminRegister).subscribe((respond)=>{
+      console.log(respond)
+      Swal.fire("Thank You...", 'You registered sucessfully', 'success')
+    },(error)=>{
+      console.log(error)
+    })
+  }
 
 }
  
