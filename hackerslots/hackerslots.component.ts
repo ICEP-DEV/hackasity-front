561c3489b8ca00274d4e9623902dfa01d9703d33
mashabelaherold <95069470+mashabelaherold@users.noreply.github.com>
2023-08-30 15:27:42 +0200

    Add files via upload

--- hackerslots/hackerslots.component.ts
+++ hackerslots/hackerslots.component.ts
@@ -4,41 +4,41 @@ import { Router } from '@angular/router';
 import swal from "sweetalert2";
 
 @Component({
-  selector: 'app-slots',
-  templateUrl: './slots.component.html',
-  styleUrls: ['./slots.component.css']
+  selector: 'app-hackerslots',
+  templateUrl: './hackerslots.component.html',
+  styleUrls: ['./hackerslots.component.css']
 })
+export class HackerslotsComponent {
 
-export class SlotsComponent {
+//   timeslot=["00:00 AM","01:00 AM","02:00 AM","03:00 AM", "04:00 AM","05:00 AM", "06:00 AM","07:00 AM",
+//   "08:00 AM","09:00 AM","10:00 AM","11:00 AM", "12:00 AM", "13:00 PM","14:00 PM", "15:00 PM", "16:00 PM", "17:00 PM",
+//   "18:00 PM", "19:00 PM", "20:00 PM","21:00 PM","22:00 PM","23:00 PM"
+// ]
 
-  // constructor(private _router:Router,){};
-
-  dropdownn(event: any){
-    console.log(event.target.value)
-    this._router.navigate(['/'+event.target.value])
-  }
+// timeslotE=["00:00 AM","01:00 AM","02:00 AM","03:00 AM", "04:00 AM","05:00 AM", "06:00 AM","07:00 AM",
+//   "08:00 AM","09:00 AM","10:00 AM","11:00 AM", "12:00 AM", "13:00 PM","14:00 PM", "15:00 PM", "16:00 PM", "17:00 PM",
+//   "18:00 PM", "19:00 PM", "20:00 PM","21:00 PM","22:00 PM","23:00 PM"
+// ]
   constructor(private _router:Router,private sityservice: SityService){};
 
   dropdown(event: any){
     console.log(event.target.value)
     this._router.navigate(['/'+event.target.value])
   }
-  selectedName: any;
-  temp:any;
-
+  selectedName:any;
+  temp : any;
   slot= {
-    judge_name:"",
-    start_time: "", end_time:"", date: "", judge_id:"", length:"1",link:"",Admin_id:""
+    group_name:"",
+    start_time: "", end_time:"", date: "",  length:"1",link:"",Admin_id:""
   }
-   
   ngOnInit(){
-    this.getNames()
+    this. getGroups()
   
 
   }
 
-  getNames(){
-    this.sityservice.getJudgeNames().subscribe((respond)=>{
+  getGroups(){
+    this.sityservice.getGroupNames().subscribe((respond)=>{
     this.temp = respond
     this.selectedName= this.temp.results
     console.log(this.selectedName)
@@ -58,19 +58,18 @@ export class SlotsComponent {
     console.log(event.target.value)
     this.slot.date=event.target.value
   }
-   adminId:any;
+    adminId: any
   addSlot(){
     swal.fire("You Assigned sucessfully")
     console.log(this.adminId= localStorage.getItem("adminID"))
     this.slot.Admin_id = this.adminId
     console.log(this.slot)
     
-    this.sityservice.judgeSlot(this.slot).subscribe((respond)=>{
+    this.sityservice.hackerSlot(this.slot).subscribe((respond)=>{
       console.log(respond)
     },(error)=>{
       console.log(error)
     })
        
   }
-
 }
